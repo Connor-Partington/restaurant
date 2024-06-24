39ae6a6262e6603c5d34ac24677fe835e513e19c
Connor <119803598+Connor-Partington@users.noreply.github.com>
2024-06-24 21:16:53 +0100

    added test function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import _ from 'lodash';
+import _ from "lodash";
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(["Hello", "webpack1"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+document.getElementById("home-button").addEventListener("click", testAlert);
+
+function testAlert() {
+    alert("Working!");
+}
