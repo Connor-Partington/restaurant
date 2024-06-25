e2e9d03c73b23776fc50640c5dbc673f9e12d6a2
Connor <119803598+Connor-Partington@users.noreply.github.com>
2024-06-25 21:25:37 +0100

    added call to homepage function to display homepage on load, and added loop to add class to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import { showHomepage } from "./home";
 import { showMenu } from "./menu";
 import { showAbout } from "./about";
+import './styles.css';
 
 document.addEventListener("DOMContentLoaded", (event) => {
+    showHomepage();
+
     document.getElementById("home-button").addEventListener("click", showHomepage);
     document.getElementById("menu-button").addEventListener("click", showMenu);
     document.getElementById("about-button").addEventListener("click", showAbout);
+
+    const navBtn = document.querySelector('nav').querySelectorAll('button');
+    navBtn.forEach(button => {
+        button.classList.add("nav-button");
+    });
 });
