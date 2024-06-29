e693681737341370e149abc89c3d0c5cc1aa7029
Connor <119803598+Connor-Partington@users.noreply.github.com>
2024-06-29 11:36:36 +0100

    removed console log script

--- src/about.js
+++ src/about.js
@@ -1,5 +1,4 @@
 export function showAbout() {
-    console.log("about ready");
     const content = document.getElementById("content");
     content.innerHTML = `<h2>This is the about page</h2>`;
 }
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,4 @@
 export function showMenu() {
-    console.log("menu ready");
     const content = document.getElementById("content");
     content.innerHTML = `<h2>This is the menu</h2>`;
 }
